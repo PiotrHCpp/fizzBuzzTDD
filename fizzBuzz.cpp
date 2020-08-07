bab7dfc892155c8d07476131a37bf6acce770632
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-08-07 12:54:09 +0200

    can call fizzBuzz function

---
+++ fizzBuzz.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include <string>
+
+std::string fizzBuzz(unsigned value) {
+    return "";
+}
+
+TEST(FizzBuzzTests, canCallFizzBuzz) {
+    std::string result = fizzBuzz(1);
+}
